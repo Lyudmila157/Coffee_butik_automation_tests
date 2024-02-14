e0fd790a4db6226f19208b1c9741ec062e6b5606
Lyudmila157 <LyudmilaK20@gmail.com>
2024-02-14 14:27:20 +0300

    First CI push

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def driver(request):
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1024,768")
     options.add_argument("--user-data-dir=chrome-data")
-    driver = webdriver.Chrome('chromedriver.exe', options=options)
+    driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
     driver.quit()
